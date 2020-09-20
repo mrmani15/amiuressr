f6faf1b23b8a2eb9e7a16df11dbe229b5bf66eeb
Manish Kumar <mk778281@gmail.com>
2020-09-20 18:04:33 +0530

    mysqlconnected

--- index.js
+++ index.js
@@ -1,7 +1,22 @@
 const express = require('express')
+const mysql = require('mysql')
 
 const app = express()
 
-app.get('/', (req, res) => res.send('api running'))
+const db = mysql.createConnection({
+  host: 'localhost',
+  user: 'root',
+  password: 'Mr@mani_15',
+  database: 'amiuressr',
+})
+
+db.connect((err) => {
+  if (err) {
+    console.log(err)
+  } else {
+    console.log('mysql connected')
+  }
+})
+app.get('/', (req, res) => res.send('api is running'))
 
 app.listen(5000, () => console.log('server started'))
